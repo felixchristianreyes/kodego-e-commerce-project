366ecdfb3580cce5b4eab203eafb1d608e2c2928
Ruzell11 <johnruzell123@gmail.com>
2022-06-20 20:41:12 +0800

    finish cart functionality and name functionality

--- login.js
+++ login.js
@@ -6,19 +6,21 @@ const inputEmail = document.querySelector('.email');
 const inputNameContainer = document.querySelector('.nameContainer');
 const inputPassConatainer = document.querySelector('.paswordContainer');
 const input = document.querySelectorAll('.input')
+var nameUser = document.getElementById('name')
 
 
 const isEmpty = () =>{
     var email = "ruzell@gmail.com"
     var password = "ruzell"
 
-    if( inputPass.value && inputEmail.value ){
+    if( inputPass.value && inputEmail.value && nameUser.value ){
         button.removeAttribute("disabled")
         button.addEventListener('click' , () =>{
             if( inputPass.value === password && inputEmail.value === email){
                 window.location.href = "homepage.html"
                 inputEmail.classList.remove('redBorder')
                 inputPass.classList.remove('redBorder')
+               nameValue()
             }
             else{
                 inputNameContainer.innerHTML = "Incorrect Email or password"
@@ -36,3 +38,10 @@ const isEmpty = () =>{
 form.addEventListener('submit' , (e) =>{
     e.preventDefault()
 })
+
+const nameValue = () => {
+    var name1 = document.getElementById('name').value;
+    localStorage.setItem('textvalue',name1 );
+    return false
+}
+
