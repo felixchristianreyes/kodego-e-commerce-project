65216c2d4a83dfda626f63d53e2c0b938bee7437
Christian <felix.christian.reyes@gmail.com>
2022-06-22 18:05:05 +0800

    fixed some login bugs

--- login.js
+++ login.js
@@ -1,48 +1,40 @@
-const form = document.getElementById('form');
-const button = document.querySelector('.btn');
-const inputName = document.querySelector('.name');
-const inputPass = document.querySelector('.password');
-const inputEmail = document.querySelector('.email');
-const inputNameContainer = document.querySelector('.nameContainer');
-const inputPassConatainer = document.querySelector('.paswordContainer');
-const input = document.querySelectorAll('.input')
-var nameUser = document.getElementById('name')
+const form = document.getElementById("form");
+const button = document.querySelector(".btn");
+const inputName = document.querySelector(".name");
+const inputPass = document.querySelector(".password");
+const inputEmail = document.querySelector(".email");
+const inputNameContainer = document.querySelector(".nameContainer");
+const inputPassConatainer = document.querySelector(".paswordContainer");
+const input = document.querySelectorAll(".input");
+var nameUser = document.getElementById("name");
 
+const isEmpty = () => {
+  var email = "user@gmail.com";
+  var password = "user1234";
 
-const isEmpty = () =>{
-    var email = "user@gmail.com"
-    var password = "user1234"
-
-    if( inputPass.value && inputEmail.value && nameUser.value ){
-        button.removeAttribute("disabled")
-        button.addEventListener('click' , () =>{
-            if( inputPass.value === password && inputEmail.value === email){
-                window.location.href = "homepage.html"
-                inputEmail.classList.remove('redBorder')
-                inputPass.classList.remove('redBorder')
-                inputNameContainer.innerHTML = ""
-               nameValue()
-            }
-            else{
-                inputNameContainer.innerHTML = "Incorrect Email or password"
-                inputEmail.classList.add('redBorder')
-                inputPass.classList.add('redBorder')
-              
-            } 
-        })
-    }
-    else{
-        button.disabled = true
-    }
-
-}
-form.addEventListener('submit' , (e) =>{
-    e.preventDefault()
-})
+  if (inputPass.value && inputEmail.value) {
+    button.removeAttribute("disabled");
+    button.addEventListener("click", () => {
+      if (inputPass.value === password && inputEmail.value === email) {
+        window.location.href = "homepage.html";
+        inputEmail.classList.remove("redBorder");
+        inputPass.classList.remove("redBorder");
+        inputNameContainer.innerHTML = "";
+        nameValue();
+      } else {
+        inputNameContainer.innerHTML = "Incorrect Email or Password";
+        inputEmail.classList.add("redBorder");
+        inputPass.classList.add("redBorder");
+      }
+    });
+  }
+};
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
 const nameValue = () => {
-    var name1 = document.getElementById('name').value;
-    localStorage.setItem('textvalue',name1 );
-    return false
-}
-
+  var name1 = document.getElementById("name").value;
+  localStorage.setItem("textvalue", name1);
+  return false;
+};
